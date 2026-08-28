8ceac84386229d9385488e19d828d2eddb72e655
autumnc <yangwenzu@163.com>
2026-08-29 00:11:50 +0800

    Bump version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pjournal.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.4.0"
+        versionCode = 9
+        versionName = "1.5.0"
     }
 
     signingConfigs {
